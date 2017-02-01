94db8b63d11ee3c06c28dbcfd177b89c4e19ddb4
Apichaya Tiwcharoensakul <apichaya.ti@ku.th>
2017-02-01 11:35:44 +0700

    Update GuessingGame

--- src/GameConsole.java
+++ src/GameConsole.java
@@ -2,29 +2,33 @@ import java.util.Scanner;
 
 /**
  * Game of guessing a secret number
+ * 
  * @author Apichaya
  *
  */
-public class GameConsole
-{
+public class GameConsole {
 	/**
 	 * play game on the console using input from a user.
-	 * @param game that i want to play
+	 * 
+	 * @param game
+	 *            that i want to play
 	 * @return the secret number when user guesses it right.
 	 */
 	public int play(GuessingGame game) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println(game.getHint());
+		int number;
 		while (true) {
 			System.out.print("What is your guess? ");
 			int num = scan.nextInt();
-			if(game.guess(num)) {
+			if (game.guess(num)) {
 				System.out.println(game.getHint());
+				number = num;
 				break;
 			}
 			System.out.println(game.getHint());
 		}
-		return game.getSecret();	
+		return number;
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 /**
  * Game of guessing a secret number
+ * 
  * @author Apichaya
  *
  */
-public class Main
-{
+public class Main {
 	/**
-	 * create objects, connect user interface to the game, and start the user interface.
-	 * main is static method.
+	 * create objects, connect user interface to the game, and start the user
+	 * interface. main is static method.
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		GuessingGame game = new GuessingGame(20);
 		GameConsole ui = new GameConsole();
-		ui.play(game);	
-	}	
+		ui.play(game);
+	}
 
 }
